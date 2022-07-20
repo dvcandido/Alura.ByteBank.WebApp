1820cbf1fa2e4ad99563dffe2f2cbe5fcc44f5d3
Dougals Vaz Candido <douglasvaz@hotmail.com>
2022-07-19 23:11:43 -0300

    Aula 04 - Testando com localizadores

--- Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -4,8 +4,10 @@ using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Alura.ByteBank.WebApp.Testes
 {
@@ -14,11 +16,14 @@ namespace Alura.ByteBank.WebApp.Testes
         public IWebDriver driver { get; private set; }
         public IDictionary<String, Object> vars { get; private set; }
         public IJavaScriptExecutor js { get; private set; }
-        public NavegandoNaPaginaHome()
+        private ITestOutputHelper SaidaConsoleTeste;
+
+        public NavegandoNaPaginaHome(ITestOutputHelper saidaConsoleTeste)
         {
             driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             js = (IJavaScriptExecutor)driver;
             vars = new Dictionary<String, Object>();
+            SaidaConsoleTeste = saidaConsoleTeste;
         }
 
 
@@ -104,6 +109,74 @@ namespace Alura.ByteBank.WebApp.Testes
             //Assert
             Assert.Contains("https://localhost:44309/Agencia/Index", driver.Url);
             Assert.Contains("401", driver.PageSource);
+        }
+
+        [Fact]
+        public void RealizaLoginAcessaMenuECadastraCliente()
+        {
+            //Arrange
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Name("Email"));
+            var senha = driver.FindElement(By.Name("Senha"));
+
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            driver.FindElement(By.LinkText("Cliente")).Click();
+            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
+
+            driver.FindElement(By.Name("Identificador")).Click();
+            driver.FindElement(By.Name("Identificador")).SendKeys("b96f2ac2-b142-41ae-ab81-98eb8d783436");
+            driver.FindElement(By.Name("CPF")).Click();
+            driver.FindElement(By.Name("CPF")).SendKeys("69981034096");
+            driver.FindElement(By.Name("Nome")).Click();
+            driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
+            driver.FindElement(By.Name("Profissao")).Click();
+            driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
+
+            //Act
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            driver.FindElement(By.LinkText("Home")).Click();
+
+            //Assert
+            Assert.Contains("Logout",driver.PageSource);
+        }
+
+        [Fact]
+        public void RealizaLoginAcessaListagemDeContas()
+        {
+            //Arrange
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Name("Email"));
+            var senha = driver.FindElement(By.Name("Senha"));
+
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+
+            driver.FindElement(By.Id("btn-logar")).Click();
+
+            driver.FindElement(By.Id("contacorrente")).Click();
+            
+            //Act
+            var elements = driver.FindElements(By.TagName("a"));
+
+            //foreach (var e in elements)
+            //{
+            //    SaidaConsoleTeste.WriteLine(e.Text);
+            //}
+
+            var elemento = (from webElemento in elements
+                            where webElemento.Text.Contains("Detalhe")
+                            select webElemento).First();
+            //Act
+            elemento.Click();
+
+            //Assert
+            //Assert.True(elements.Count >= 12);
+            Assert.Contains("Voltar", driver.PageSource);
         }
 
 
